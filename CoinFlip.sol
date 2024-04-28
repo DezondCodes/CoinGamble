0a75a4fe6dbf0434a817137bf306b38e9acb5917
dezondcodes <projectdezond@gmail.com>
2024-04-28 16:57:12 +0330

    Constructor & 1stFunction

--- CoinFlip.sol
+++ CoinFlip.sol
@@ -14,4 +14,15 @@ uint256 public FEE;
 mapping (uint256 => address payable ) public players;
 mapping (uint256 => uint256) public bets;
 
+constructor(address payable _coordinator, bytes32 _keyHash, uint256 _fee) VRFConsumerBaseV2(_coordinator) public  {
+    COORDINATOR = VRFCoordinatorV2Interface(_coordinator);
+    KEYHASH = _keyHash;
+    FEE = _fee;
+}
+
+function flipCoin(address payable _player, uint256 _betAmount) payable public {
+    require(_betAmount > 0, "Bet amount should be more than 0");
+    (bool success, ) = payable(address(this)).call{value: _betAmount}("");
+    require(success, "Tranfer Failed");
+}
 } 
